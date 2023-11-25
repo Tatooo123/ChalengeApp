08b2bef616d44f1bdecc0c08aba19e2b79e7a345
Tatooo123 <stankowski@wr.onet.pl>
2023-11-25 12:17:43 +0100

    Homework - If...else statement (commit no 4) Day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,39 +1,35 @@
-﻿var myAge = 58;
-int adamsAge = 33;
-int newAge;
-int maxVal = int.MaxValue;
-var countryName = "Patagonia";
-string name = "Adam";
+﻿var xName = "Jaromir";
+var xAge = 17;
+var xIsWoman = false;
 
-Console.WriteLine("Hello there. Clap hands! I'm " + myAge + " years old, but Adam is " + adamsAge);
-Console.WriteLine("maxVal " + maxVal);
-maxVal = +1;
-Console.WriteLine("maxVal " + maxVal + countryName);
-newAge = myAge + adamsAge;
-Console.WriteLine("newAge " + newAge);
-// nw typy danych
-Console.WriteLine("--------------------");
-//      dla typu bool NIE MA właściwości Min i MaxValue
-//      Console.WriteLine("bool    - " + bool.MaxValue + " do " + bool.MinValue);
-Console.WriteLine("int     - " + int.MinValue + " do " + int.MaxValue);
-Console.WriteLine("uint    - " + uint.MinValue + " do " + uint.MaxValue);
-Console.WriteLine("decimal - " + decimal.MinValue + " do " + decimal.MaxValue);
-Console.WriteLine("float   - " + float.MinValue + " do " + float.MaxValue);
-Console.WriteLine("double  - " + double.MinValue + " do " + double.MaxValue);
-Console.WriteLine("char    - " + char.MinValue + " do " + char.MaxValue);
-// KONIECZNA zmiana typu danych do porównania lub wykonania operacji
-//      Console.WriteLine(decimal.MaxValue > float.MaxValue);
-//      Console.WriteLine(decimal.MaxValue + float.MaxValue);
-
-if (adamsAge > 50 && name == "Adam")
+if (xIsWoman && xAge < 30)
 {
-    Console.WriteLine(" Adam jest starszy niż 50 lat ");
+    Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (name == "Adam")
+else if (xName == "Ewa" && xAge == 33)
 {
-    Console.WriteLine(" Adam nie jest starszy niż 50 lat ");
+    Console.WriteLine(xName + ", lat " + xAge);
+}
+else if (!xIsWoman && xAge < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if ((xName == "Adam" || xName == "Ewa") && xAge > 1000)
+{
+    Console.WriteLine("Prawdopodobnie mieszka w Raju");
 }
 else
 {
-    Console.WriteLine(" to nie jest Adam");
+    Console.WriteLine("Wystąpił wyjątek nieprzewidziany przez algorytm!");
 }
+Console.WriteLine("============================================");
+Console.WriteLine("imię:    " + xName);
+if (xIsWoman)
+{
+    Console.WriteLine("płeć:    kobieta");
+}
+else
+{
+    Console.WriteLine("płeć:    mężczyzna");
+}
+Console.WriteLine("wiek:    " + xAge);
