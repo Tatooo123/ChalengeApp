8043411f1fe545c0afa2d5dfbc9df2715df229d1
Tatooo123 <stankowski@wr.onet.pl>
2023-11-25 11:57:00 +0100

    Next changes in Hello World... (Commit no 2) - Day 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! My name is Mark");
+var myAge = 58;
+int adamsAge = 33;
+int newAge;
+int maxVal = int.MaxValue;
+var countryName = "Patagonia";
+string name = "Adam";
+
+Console.WriteLine("Hello there. Clap hands! I'm " + myAge + " years old, but Adam is " + adamsAge);
