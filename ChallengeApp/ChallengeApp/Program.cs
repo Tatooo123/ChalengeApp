f2845b9c0c7b53626b4c27388b15604173f16eee
Tatooo123 <stankowski@wr.onet.pl>
2023-11-27 14:36:36 +0100

    Homework Day 5 v3 - alternate (commit no 10)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,15 @@
-﻿int number = 982891;
+﻿int number = 119983830;
 string numberAsString = number.ToString();
 char[] letters = numberAsString.ToCharArray();
 int[] counters = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-// int val0;
-// val0 = Convert.ToInt32("0"); - why always returns 0, not 48
 
 foreach (char letter in letters)
 {
-//    counters[Convert.ToInt32(letter) - 48]++;
-    counters[letter - '0']++;
+    counters[Convert.ToInt32(letter) - Convert.ToInt32('0')]++;
+//    counters[letter - '0']++;
 }
 for (int i = 0; i < counters.Length; i++)
 {
     Console.WriteLine(i+" => "+counters[i]);
 }
+Console.WriteLine("Finish");
