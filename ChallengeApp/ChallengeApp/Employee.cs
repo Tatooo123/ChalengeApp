9d37470dca0f10adf944876a370342e65639e7b7
Tatooo123 <stankowski@wr.onet.pl>
2023-12-13 00:29:46 +0100

    Day 6 homework

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace ChallengeApp;
+
+public class Employee
+{
+    public Employee(string firstName, string lastName, int age) 
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    private List<int> points = new List<int>();
+    public void AddPoints(int score)
+    { 
+        this.points.Add(score);
+    }
+    public int Result
+    {
+        get
+        { 
+            return this.points.Sum(); 
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,15 +1,47 @@
-﻿int number = 119983830;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToCharArray();
-int[] counters = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+﻿using ChallengeApp;
 
-foreach (char letter in letters)
+Employee worker1 = new Employee("Tom", "Jones", 24);
+Employee worker2 = new Employee("Susie", "Jones", 29);
+Employee worker3 = new Employee("Mark", "Kowalski", 39);
+Employee worker4 = new Employee("Dorothy", "Longbutton", 41);
+int maxScore = -1;
+Employee maxScoreWorker = null;
+List<Employee> workers = new List<Employee>()
+{ 
+    worker1, worker2, worker3, worker4 
+};
+
+worker1.AddPoints(10);
+worker1.AddPoints(7);
+worker1.AddPoints(2);
+worker1.AddPoints(1);
+worker1.AddPoints(5);
+
+worker2.AddPoints(1);
+worker2.AddPoints(8);
+worker2.AddPoints(5);
+worker2.AddPoints(3);
+worker2.AddPoints(9);
+
+worker3.AddPoints(3);
+worker3.AddPoints(6);
+worker3.AddPoints(1);
+worker3.AddPoints(9);
+worker3.AddPoints(7);
+
+worker4.AddPoints(1);
+worker4.AddPoints(4);
+worker4.AddPoints(10);
+worker4.AddPoints(3);
+worker4.AddPoints(6);
+
+foreach (var worker in workers)
 {
-    counters[Convert.ToInt32(letter) - Convert.ToInt32('0')]++;
-//    counters[letter - '0']++;
+    if ( worker.Result > maxScore )
+    { 
+        maxScore = worker.Result;
+        maxScoreWorker = worker;
+    }
 }
-for (int i = 0; i < counters.Length; i++)
-{
-    Console.WriteLine(i+" => "+counters[i]);
-}
-Console.WriteLine("Finish");
+
+Console.WriteLine(maxScoreWorker.FirstName + " " + maxScoreWorker.LastName + " age " + maxScoreWorker.Age + " score: " + maxScore);
